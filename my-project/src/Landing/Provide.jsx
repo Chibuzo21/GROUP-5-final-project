85b3f57c28041b981ae78a032258e0276d221f7f
Chibuzo <uchibuzo1@gmail.com>
2024-06-13 13:29:55 +0100

    gggh

--- my-project/src/Landing/Provide.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Provide() {
-  return (
-    <div>Provide</div>
-  )
-}
-
-export default Provide
--- my-project/src/Pages/Carekonect.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Carekonect() {
-  return (
-    <div>Carekonect</div>
-  )
-}
-
-export default Carekonect
