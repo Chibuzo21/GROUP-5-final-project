4dcb9ffba1ccf78a9101ffbce88e9dff2b6867eb
codewhite777 <ifeanyiogbonnaya777@gmail.com>
2024-11-06 15:56:50 +0100

    testing the user model

--- BACKEND/CareKonect/User/models.py
+++ BACKEND/CareKonect/User/models.py
@@ -2,3 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Appointment(models.Model):
+    patient_name = models.CharField(max_length=100)
+    doctor_name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.patient_name} - {self.date} at {self.time}"
