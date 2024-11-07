b039e8352e8dd6b0f77355bcba9c6e807ca3f3ee
codewhite777 <ifeanyiogbonnaya777@gmail.com>
2024-11-07 14:10:05 +0100

    correction the api own is authenticating i'm trying to make the user pass a correct information before he will be allowed to login

--- BACKEND/CareKonect/User/views.py
+++ BACKEND/CareKonect/User/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny,  IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib import messages
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.models import User,auth 
+
+
 
 from .serializers import UserSerializer, LoginSerializer
 
@@ -14,11 +19,13 @@ class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
+    
+    
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
     serializer_class = LoginSerializer
-    # queryset = User.objects.all()
+    queryset = User.objects.all()
     
 
     def post(self, request, *args, **kwargs):
@@ -35,5 +42,7 @@ class LoginView(APIView):
                 'access': str(refresh.access_token),
             })
         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
-
-
+    
+    
+    
+       
