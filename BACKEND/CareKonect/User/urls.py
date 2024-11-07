1b2ebb1bdfc746c0f2fab1b8fe9d70b99e8c76a7
codewhite777 <ifeanyiogbonnaya777@gmail.com>
2024-11-07 11:09:54 +0100

    testing the authenticity of the login and signup

--- BACKEND/CareKonect/User/urls.py
+++ BACKEND/CareKonect/User/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import SignupView, LoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', auth_views.SignupView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
