68cd715b9e3b83978419a7c7e334391a9e577609
codewhite777 <ifeanyiogbonnaya777@gmail.com>
2024-11-07 15:50:25 +0100

    user database testing

--- BACKEND/CareKonect/User/models.py
+++ BACKEND/CareKonect/User/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Appointment(models.Model):
-    patient_name = models.CharField(max_length=100)
-    doctor_name = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.TimeField()
-    description = models.TextField()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_doctor = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
 
     def __str__(self):
-        return f"{self.patient_name} - {self.date} at {self.time}"
+        return self.user.username
